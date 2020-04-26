94f10a30dbc823cb90c9f81ccb61a5aafa12084a
Alain D'Ettorre <alain.det@gmail.com>
2020-04-27 00:41:09 +0200

    Add first core function test

--- tests/sass-pal.spec.js
+++ tests/sass-pal.spec.js
@@ -4,13 +4,10 @@ const path = require('path')
 const sassTrue = require('sass-true')
 const glob = require('glob')
 
-describe('Sass', () => {
-  // Find all of the Sass files that end in `*.spec.scss` in any directory of this project.
-  // I use path.resolve because True requires absolute paths to compile test files.
-  const sassTestFiles = glob.sync(path.resolve(process.cwd(), 'tests/**/*.spec.scss'))
-
-  // Run True on every file found with the describe and it methods provided
-  sassTestFiles.forEach(file =>
-    sassTrue.runSass({ file }, { describe, it })
-  )
-})
+describe('Sass-Pal', () => {
+  const testPath = path.resolve(process.cwd(), 'tests/sass-pal.spec.scss');
+  const files = glob.sync(testPath);
+  for (const file of files) {
+    sassTrue.runSass({ file }, { describe, it });
+  }
+});
