8f03f5de59b6538d631df2003009bc57357bfbf9
Alain D'Ettorre <alain.det@gmail.com>
2020-05-01 18:15:33 +0200

    Fix online doc script

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -12,27 +12,27 @@ fi
 echo "${PREFIX} Version passed: ${1}"
 
 echo "${PREFIX} 01/08 # Checkout to gh-pages branch"
-# git checkout gh-pages
+git checkout gh-pages
 
 echo "${PREFIX} 02/08 # Merge with master branch"
-# git merge master
+git merge master
 
 echo "${PREFIX} 03/08 # Generate documentation"
-# npm run doc
+npm run doc
 
 echo "${PREFIX} 04/08 # Cleanup old documentation"
-# rm -rf ./assets ./index.html
+rm -rf ./assets ./index.html
 
 echo "${PREFIX} 05/08 # Move new documentation to root"
-# mv ./doc/assets/ ./assets/
-# mv ./doc/index.html ./index.html
+mv ./doc/assets/ ./assets/
+mv ./doc/index.html ./index.html
 
 echo "${PREFIX} 06/08 # Commit and push new documention"
-# git add .
-# git commit -m "${PREFIX} Update documentation version ${1}"
-# git push origin gh-pages
+git add .
+git commit -m "${PREFIX} Update documentation version ${1}"
+git push origin gh-pages
 
 echo "${PREFIX} 07/08 # Switch back to master branch"
-# git checkout master
+git checkout master
 
 echo "${PREFIX} 08/08 # Finished"
