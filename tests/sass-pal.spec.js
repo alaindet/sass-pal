679ee96fce8037b1d43f95e3ca371d5f904e801c
Alain D'Ettorre <alain.det@gmail.com>
2021-02-21 11:59:41 +0100

    Move first test to unit folder

--- tests/sass-pal.spec.js
+++ tests/sass-pal.spec.js
@@ -1,12 +1,13 @@
 // https://www.educative.io/blog/sass-tutorial-unit-testing-with-sass-true
 
-const path = require('path')
-const sassTrue = require('sass-true')
-const glob = require('glob')
+const path = require('path');
+const glob = require('glob');
+const sassTrue = require('sass-true');
 
 describe('Sass-Pal', () => {
-  const testPath = path.resolve(process.cwd(), 'tests/sass-pal.spec.scss');
-  const files = glob.sync(testPath);
+  const srcDir = path.join(process.cwd(), 'src');
+  const globPattern = `${srcDir}/**/*.spec.scss`;
+  const files = glob.sync(globPattern);
   for (const file of files) {
     sassTrue.runSass({ file }, { describe, it });
   }
